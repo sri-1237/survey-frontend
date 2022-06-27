1e9cafcad16da9b193065f0b718d92afaf36ec20
sri-1237 <105025206+sri-1237@users.noreply.github.com>
2022-06-27 12:59:11 -0500

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-
-
 module.exports = {
   devServer: {
-    host: 'localhost',
     port: 8081
   },
   publicPath: process.env.NODE_ENV === 'development' ? '/' : '/survey-frontend/',
